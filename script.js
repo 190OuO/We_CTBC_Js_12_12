87b7f2d064d90c31fbfb1826ee66a65c929f3679
190OuO <0857banana@gmail.com>
2024-12-12 14:04:34 +0800

    Updata 建立基本架構

--- script.js
+++ script.js
@@ -29,4 +29,62 @@ console.log(fonColor);
 test =9;
 console.log(test);
 
+// 資料類型
+let VarNumber =1.5;   //數值
+let VarString ="字串";//字串
+let varBool = true;   //布林值(true、false)
+
+// typeof 獲得資料型態
+console.log(VarNumber);
+console.log(VarString);
+console.log(varBool);
+
+// 運算子 
+// 加、減、乘、除、餘、次方(+,-,*,/,%,**)
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3);   // 10除以3=3餘1
+                        // 10除以3次方
+
+// 指定運算子 (指定符號=)
+let numberTest = 99 + 1;
+console.log(numberTest);
+numberTest++;
+console.log(numberTest);
+numberTest--;
+console.log(numberTest);
+numberTest+=10;          // 適用於+-*%  
+console.log(numberTest);
+numberTest-=10; 
+console.log(numberTest);
+
+console.log("%c比較運算子","color: gold");
+// > < >= <= === !== ( 執行結果是布林值)
+console.log(100 > 1);
+console.log(100 < 1);
+console.log(100 === 1); //等於F
+console.log(100!==1);   //不等於 T
+
+//  == 與 === (嚴格比較，數值與類型都要相同)
+
+let VarNumber10 =10;
+let VarString10 ="10";
+
+console.log(VarNumber10 == VarString10);
+console.log(VarNumber10 === VarString10);      //嚴格比較，資料類型不同，結果為false
+
+console.log("%c判斷式","color:gold");
+// 判斷式語法
+// if (布林值){ 布林值等於 true 執行此區塊 block }
+if (false) {             //if (假如是false 則不會執行 且字顏色會變淡 )
+    console.log("這裡是判斷式");  // 這邊
+}
+
+
+
+
+
 
